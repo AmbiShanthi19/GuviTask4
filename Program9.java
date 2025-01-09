d4ae893892663b6ecd635fcc5aad232eb07b1fcc
AmbiShanthi19 <ambishanthi19@gmail.com>
2025-01-09 22:35:32 +0530

    Programs of Task4

---
+++ Program9.java
@@ -0,0 +1,17 @@
+
+
+package task4;
+
+public class Program9 {
+
+	public static void main(String[] args) {
+		int age = 65;
+		if(age>60) {
+			System.out.println("The Person is senior Citizen ");
+		}
+		else {
+			System.out.println("The person is not senior citizen ");
+		}
+	}
+
+}
