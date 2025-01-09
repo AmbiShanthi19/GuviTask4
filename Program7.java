daaa6c1b2b58df842eb9d4973db5a8f097e162d5
AmbiShanthi19 <ambishanthi19@gmail.com>
2025-01-09 22:33:48 +0530

    Programs of Task4

---
+++ Program7.java
@@ -0,0 +1,13 @@
+
+
+package task4;
+
+public class Program7 {
+
+	public static void main(String[] args) {
+		
+		String msg="Guvi Geek";
+		System.out.println("The length of the string is " + msg.length());
+	}
+
+}
